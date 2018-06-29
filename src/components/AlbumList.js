868c3538e39a72f992e90508fcd95ed772bb9c2b
Facundo García Reyna <facundo.rgarcia@mercadolibre.com>
2018-06-29 17:56:08 -0300

    handle list refresh

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -5,7 +5,11 @@ import AlbumDetail from './AlbumDetail';
 import { List } from "react-native-elements";
 
 class AlbumList extends Component {
-  state = { photoset: null, user_id: "60216816%40N00" };
+  state = { 
+    photoset: null, 
+    user_id: "60216816%40N00",
+    refreshing: false 
+   };
 
   componentWillMount() {
     this.makeRemoteRequest()
@@ -13,7 +17,20 @@ class AlbumList extends Component {
 
   makeRemoteRequest = () => {
     axios.get(`https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=${this.state.user_id}&format=json&nojsoncallback=1`)
-    .then(response => this.setState({ photoset: response.data.photosets.photoset }));
+    .then(response => this.setState({ 
+      photoset: response.data.photosets.photoset, 
+      refreshing: false 
+    }));
+  }
+
+  handleRefreshing = () => {
+    this.setState({ 
+      refreshing: false 
+    },
+    () => {
+      this.makeRemoteRequest();
+    }
+  );
   }
 
   render() {
@@ -33,6 +50,8 @@ class AlbumList extends Component {
           <AlbumDetail key={item.id} title={item.title._content}  albumId={item.id} userId={this.state.user_id} />
         )}
         keyExtractor={item => item.id}
+        onRefresh={this.handleRefreshing}
+        refreshing={this.state.refreshing}
         />
       </List>
     );
--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -5,7 +5,10 @@ import PhotoDetail from './PhotoDetail';
 import { List } from "react-native-elements";
 
 class PhotoList extends Component {
-  state = { photos: null };
+  state = { 
+    photos: null, 
+    refreshing: false 
+  };
 
   componentWillMount() {
     this.makeRemoteRequest()
@@ -13,7 +16,20 @@ class PhotoList extends Component {
 
   makeRemoteRequest = () => {
     axios.get(`https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=6e8a597cb502b7b95dbd46a46e25db8d&photoset_id=${this.props.albumId}&user_id=${this.props.userId}&format=json&nojsoncallback=1`)
-    .then(response => this.setState({ photos: response.data.photoset.photo }));
+    .then(response => this.setState({ 
+      photos: response.data.photoset.photo, 
+      refreshing: false 
+    }));
+  }
+
+  handleRefreshing = () => {
+    this.setState({ 
+      refreshing: false 
+    },
+    () => {
+      this.makeRemoteRequest();
+    }
+  );
   }
 
   render() {
@@ -35,6 +51,8 @@ class PhotoList extends Component {
           <PhotoDetail key={item.title} title={item.title} imageUrl={`https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`} />
         )}
         keyExtractor={item => item.id}
+        onRefresh={this.handleRefreshing}
+        refreshing={this.state.refreshing}
         />
       </List>
     );
